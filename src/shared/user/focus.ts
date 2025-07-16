3b7ac70b264cfc6fd5d1241e57ac3024d047c1fd
yulin <aaayulin@gmail.com>
2025-07-16 11:00:18 +0800

    refactor: focus函数无需返回值

--- src/shared/user/focus.ts
+++ src/shared/user/focus.ts
@@ -2,13 +2,14 @@ import { agsap } from '@/shared/gsap'
 
 export function focus(name: string) {
   const dom = document.querySelector(`[${name}]`)
-  if (!dom) return true
+
   // dom.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' })
+
   agsap.timeline().to(dom, { duration: 0.15, scale: 1.1 }).to(dom, { duration: 0.15, scale: 1 })
+
   agsap.to(dom, { duration: 0.3, border: '2px dashed #424242' }).then(() => {
     setTimeout(() => {
       agsap.to(dom, { duration: 0.3, border: '2px dashed #42424200', delay: 0.3 })
     }, 600)
   })
-  return true
 }
