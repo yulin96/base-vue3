f76e14e63737170ad4e7ccc3d740083665fe81f2
yulin <aaayulin@gmail.com>
2025-09-19 10:02:26 +0800

    refactor: 精简 number-flow 组件，移除不必要的动画逻辑

--- src/components/com/number-flow.vue
+++ src/components/com/number-flow.vue
@@ -1,40 +1,15 @@
 <script setup lang="ts">
-import { continuous } from '@number-flow/vue'
-import { v4 } from 'uuid'
-import { onMounted } from 'vue'
+import NumberFlow, { continuous } from '@number-flow/vue'
 
-const props = defineProps<{ value: number; animate?: boolean }>()
-
-const id = v4()
-let dom: HTMLElement | null = null
-let color = 'null'
-
-const handleAnimationsStart = () => {
-  if (dom && props.animate) gsap.to(dom, { color: '#f00' })
-}
-
-const handleAnimationsFinish = () => {
-  if (dom && props.animate) gsap.to(dom, { color: color })
-}
-
-onMounted(() => {
-  dom = document.getElementById(id)
-  if (dom) color = getComputedStyle(dom).color
-})
+defineProps<{ value: number }>()
 </script>
 
 <template>
   <number-flow
-    :id="id"
-    :value="value"
-    will-change
-    locales="en"
-    :format="{
-      useGrouping: false,
-    }"
-    :trend="0"
     :plugins="[continuous]"
-    @animationsstart="handleAnimationsStart"
-    @animationsfinish="handleAnimationsFinish"
+    :value="value"
+    :trend="1"
+    :format="{ useGrouping: false }"
+    will-change
   ></number-flow>
 </template>
