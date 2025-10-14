02464e9203f68e12248a6e834797454056184139
yulin <aaayulin@gmail.com>
2025-10-14 16:43:54 +0800

    chore: 更新 .env 文件中的本地存储名称和 OSS 根目录配置 chore: 修改 keyboard.vue 中的弹出窗口背景类名 feat: 在 index.vue 中添加 axios 请求和测试功能

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { useLoading } from '@/hooks/useLoading'
+import { showInfoToast, showSuccessToast } from '@/shared/vant/toast'
+import axios, { toFormData } from 'axios'
 import { onMounted, onUnmounted } from 'vue'
 
 const { start, cleanup } = useLoading(window.IMG_RESOURCES ?? [])
@@ -15,12 +17,25 @@ onMounted(() => {
 onUnmounted(() => {
   cleanup()
 })
+
+const test = (type: 'password' | 'stamp', content: string) => {
+  if (type === 'password') {
+    alert(`密码：${content}`)
+  } else {
+    axios.post('https://ayp.event1.cn/api/xhsfitf/checkStamp', toFormData({ stamp: content })).then((res) => {
+      if (res.data.code == 200) return showSuccessToast('盖章成功')
+      showInfoToast(res.data?.msg || '')
+    })
+  }
+}
 </script>
 
 <template>
   <com-main>
     <section class="scroll-box index">
-      <main class="content"></main>
+      <main class="content relative p-20">
+        <com-seal-touch @next="test"></com-seal-touch>
+      </main>
     </section>
   </com-main>
 </template>
