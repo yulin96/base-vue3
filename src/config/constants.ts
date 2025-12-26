0cc59db7b1c8d4e246e320c6e00ed710b0e7184b
yulin <aaayulin@gmail.com>
2025-12-26 17:10:03 +0800

    refactor: 移除不再使用的头像获取函数

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,5 +3,3 @@ export const ossVideoSuffix = (time = 100) => `?x-oss-process=video/snapshot,t_$
 export const cosVideoSuffix = (time = 100) => `?ci-process=snapshot&time=${time}&format=jpg`
 
 export const ossWebp = '?x-oss-process=image/format,webp'
-
-export const getAvatar = (id = 1) => `https://oss.eventnet.cn/H5/zz/public/font/12/avatar/${id}.jpg`
