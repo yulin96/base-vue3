96aac5bde01b9864178fcda5d13c524af10307bf
yulin <aaayulin@gmail.com>
2026-05-13 11:32:07 +0800

    feat: 添加 @lucide/vue 依赖及其类型声明文件

---
+++ types/lucid-view.d.ts
@@ -0,0 +1,7 @@
+declare module '@lucide/vue' {
+  // Prefixed import names
+  export * from '@lucide/vue/dist/lucide-vue.prefixed'
+  // or
+  // Suffixed import names
+  // export * from '@lucide/vue/dist/lucide-vue.suffixed'
+}
