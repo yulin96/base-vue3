a7a62d86dfad8d84c8324979937c2c3e603bc98f
yulin <aaayulin@gmail.com>
2026-05-17 22:33:39 +0800

    refactor: 移除背景颜色属性，优化边框按钮组件结构和样式

--- src/components/base/border-button.vue
+++ src/components/base/border-button.vue
@@ -1,53 +1,48 @@
 <script setup lang="ts">
-defineProps<{ bgColor: string; highlight: string }>()
+defineProps<{ highlight: string }>()
 </script>
 
 <template>
-  <div class="center rounded-full p-0" :style="{ backgroundColor: bgColor }">
-    <div class="conic center h-full w-full rounded-full">
-      <slot />
-    </div>
+  <div class="center relative z-0 size-full">
+    <div v-bind="$attrs" class="conic pointer-events-none absolute inset-0 -z-10 size-full"></div>
+    <slot />
   </div>
 </template>
 
 <style scoped>
 @keyframes rotate {
   100% {
-    transform: rotate(1turn);
+    transform: translate(-50%, -50%) rotate(1turn);
   }
 }
 
 .conic {
-  overflow: hidden;
-  position: relative;
-  z-index: 20;
+  mask:
+    linear-gradient(#000 0 0) content-box,
+    linear-gradient(#000 0 0);
+
+  -webkit-mask-composite: xor;
+  mask-composite: exclude;
+
+  padding: 4px;
 }
+
 .conic::before {
   content: '';
   position: absolute;
   z-index: -2;
-  left: -50%;
-  top: -50%;
-  width: 200%;
-  height: 200%;
-  background-repeat: no-repeat;
-  background-position: 0 0;
+  left: 50%;
+  top: 50%;
+  width: 200vw;
+  height: 200vw;
+  transform: translate(-50%, -50%);
   background-image: conic-gradient(
-    transparent 0deg,
-    transparent 40deg,
-    v-bind(highlight) 110deg,
-    transparent 130deg,
-    transparent 360deg
+    v-bind(highlight) 0deg,
+    transparent 90deg,
+    v-bind(highlight) 180deg,
+    transparent 270deg,
+    v-bind(highlight) 360deg
   );
   animation: rotate 4s linear infinite;
-}
-
-.conic::after {
-  content: '';
-  position: absolute;
-  z-index: -1;
-  background-color: v-bind(bgColor);
-  border-radius: 9999px;
-  inset: 6px;
 }
 </style>
