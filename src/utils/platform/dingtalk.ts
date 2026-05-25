cef3359c41393ddd3c75aa3bd18c49ab33dd774b
yulin <aaayulin@gmail.com>
2026-05-25 16:08:07 +0800

    feat: 添加 hideDingTalkShare 函数以隐藏钉钉分享按钮

--- src/utils/platform/dingtalk.ts
+++ src/utils/platform/dingtalk.ts
@@ -1,5 +1,11 @@
-import { env } from 'dingtalk-jsapi'
+import { biz, env } from 'dingtalk-jsapi'
 
 export function isDingDing() {
   return env.platform !== 'notInDingTalk'
+}
+
+export function hideDingTalkShare() {
+  if (!isDingDing()) return
+
+  void biz.navigation.setRight({ show: false }).catch(console.log)
 }
