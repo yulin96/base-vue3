8d04d4bf79e02aea714aaf2d345b2d017269e3da
yulin <aaayulin@gmail.com>
2026-06-15 13:54:56 +0800

    wip

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import { useGsapContext } from '@/hooks/animation/useGsapContext'
+import { showScan } from '@/utils/platform/showScan'
 
 defineOptions({ name: 'Index' })
 definePage({ meta: { index: 10 } })
+
+const openScan = () => {
+  showScan().then((res) => {
+    console.log(res)
+  })
+}
 
 useGsapContext('.index', () => {
   gsap.timeline({ delay: 0.5 })
@@ -12,7 +19,9 @@ useGsapContext('.index', () => {
 <template>
   <div class="size-full">
     <section class="scroll-box index">
-      <main class="content"></main>
+      <main class="content center">
+        <div class="size-90 ring" btn @click="openScan"></div>
+      </main>
     </section>
   </div>
 </template>
