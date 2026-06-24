b26635c2befa2cd7e121a4332f40975915165913
yulin <aaayulin@gmail.com>
2026-06-24 09:57:44 +0800

    feat: 新增测试模式URL判断常量  isTestURL

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,3 +7,8 @@ export const prodModel = import.meta.env.PROD
  * 当前应用是否为开发模式
  */
 export const devModel = import.meta.env.DEV
+
+/**
+ * 当前应用是否为测试模式
+ */
+export const isTestURL = location.href.includes('__test__')
